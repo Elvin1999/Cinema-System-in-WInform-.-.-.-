278dcac17e8b3ce6340de4b55bb9d3d4635a4f6e
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-30 18:01:02 +0400

    Movie and UserMovie classes are  ready . . .

---
+++ Cinema System/UserMovie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cinema_System
+{
+    class UserMovie
+    {
+        List<Movie> usermovies = new List<Movie>();
+        List<Movie> GetList()
+        {
+            return usermovies;
+        }
+        public string FileName { get; set; }
+    }
+}
